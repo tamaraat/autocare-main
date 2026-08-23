7de38661d948558139620e7b0b68a851bcd4ed0e
Tamara Toshkova <tatoshkova@gmail.com>
2026-08-23 15:58:32 +0300

    chore: initialize main application

---
+++ src/main/java/com/autocare/AutocareMainApplication.java
@@ -0,0 +1,13 @@
+package com.autocare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocareMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutocareMainApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/autocare/config/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.autocare.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
